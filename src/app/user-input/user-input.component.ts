182fc4d45abdad81b769925553756044d2144a3b
Prajval <prajvalhl@gmail.com>
2024-11-17 19:59:14 +0530

    updated component

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  initialInvestment?: number;
+  annualInvestment?: number;
+  expectedReturn?: number;
+  duration?: number;
 
+  onFormSubmit() {
+    if (
+      this.initialInvestment &&
+      this.annualInvestment &&
+      this.expectedReturn &&
+      this.duration
+    ) {
+      console.log({
+        initialInvestment: this.initialInvestment,
+        annualInvestment: this.annualInvestment,
+        expectedReturn: this.expectedReturn,
+        duration: this.duration,
+      });
+    }
+  }
 }
